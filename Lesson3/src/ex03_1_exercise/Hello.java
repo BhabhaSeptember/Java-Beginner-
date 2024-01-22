39300e1f5b16386524a481e12d6a291e57d83843
BhabhaSeptember <septemberbhabha@gmail.com>
2024-01-22 20:11:11 +0200

    commit

---
+++ Lesson3/src/ex03_1_exercise/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex03_1_exercise;
+
+/**
+ *
+ * @author User
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        //shortcut for above: type 'main' then press CTRL+space then click enter
+        System.out.println("Hello World!");
+        //shortcut for above: type 'sout' then press CTRL+space then click enter
+    }
+
+}
